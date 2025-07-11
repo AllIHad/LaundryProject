49cf1e05ee6a6bc13822d3b34b8ef83e91077c64
leonza26 <muhammadleonza10@gmail.com>
2025-07-11 10:18:56 +0700

    add page navbar
    add page along with route and function in controller

--- app/Http/Controllers/LaundryController.php
+++ app/Http/Controllers/LaundryController.php
@@ -13,4 +13,16 @@ class LaundryController extends Controller
     public function katalogPage(){
         return view ('laundry.katalog');
     }
+
+    public function kontak_pemesananPage(){
+        return view ('laundry.kontak_pemesanan');
+    }
+
+    public function daftarmemberPage(){
+        return view ('laundry.daftar_member');
+    }
+
+    public function konfirmasipesananPage(){
+        return view ('laundry.konfirmasi_pemesanan');
+    }
 }
